92ebb605f603555a652401234937563c815ef3d0
traigiuxom9x@gmail.com <traigiuxom9x@gmail.com>
2024-06-02 21:24:15 +0700

    Initial project

---
+++ src/main/java/com/taidev198/ecomstorejavaspringmvc/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.taidev198.javabackendspringmvc.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "user/index";//find index.jsp file
+    }
+
+
+}
